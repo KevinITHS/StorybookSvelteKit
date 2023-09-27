686834ea45c9674ad6588e7c0f5348d2f16b0264
KevinITHS <kevin.ali@iths.se>
2023-09-27 15:00:06 +0200

    tghfgh

--- src/lib/components/Potato/Potato.svelte
+++ src/lib/components/Potato/Potato.svelte
@@ -1,1 +1,7 @@
-🥔
+<script lang="ts">
+export let exploded: boolean = false;
+</script>
+
+
+
+{exploded ?  "💥" : "🥔"}
---
+++ src/lib/stylesheets/stories/FontStory.svelte
@@ -0,0 +1,11 @@
+<h1 class="primary-fat-500">
+primary-fat-500
+</h1>
+
+<h2 class="primary-fat-500">
+primary-fat-500
+</h2>
+
+<p class="primary thin-100">
+    primary-thin-100
+</p>
