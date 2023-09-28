c30b4b1921f0e9253c76a1a4ff9f3a03f0a16a75
KevinITHS <kevin.ali@iths.se>
2023-09-28 12:54:11 +0200

    Download Fonts, Fix Fonts CSS etc.

--- src/lib/utilities/FontStory.svelte
+++ src/lib/utilities/FontStory.svelte
@@ -1,5 +1,30 @@
 <h1 class="primary-fat-500">primary-fat-500</h1>
 
-<h2 class="primary-fat-500">primary-fat-500</h2>
+<h2 class="primary-fat-100">primary-fat-100</h2>
 
 <p class="primary thin-100">primary-thin-100</p>
+
+<style>
+    .primary-fat-500 {
+        font-family: var(--font-family-fat);
+    }
+
+    .primary-fat-100 {
+        font-family: var(--font-family-fat);
+    }
+
+    .primary.thin-100 {
+        font-family: var(--font-family-thin);
+    }
+
+    .primary-fat-500,
+    .primary-fat-100 {
+        text-align: center;
+        padding: 1px;
+    }
+
+    .primary.thin-100 {
+        padding-top: 20px;
+        text-align: center;
+    }
+</style>
